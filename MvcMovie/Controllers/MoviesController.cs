e163c5aa7e7812d20545d719daf480cc4ddc63c6
Alexandru Rotariu <alexandru.rotariu@emanuel.ro>
2021-03-08 21:14:48 +0200

    Fixed problems

--- MvcMovie/Controllers/MoviesController.cs
+++ MvcMovie/Controllers/MoviesController.cs
@@ -15,13 +15,11 @@ namespace MvcMovie.Controllers
 
         static List<Movie> movies = new List<Movie>();
 
-        // GET: Movies
         public IActionResult Index()
         {
             return View(movies);
         }
 
-        // GET: Movies/Details/5
         public IActionResult Details(Guid? id)
         {
             if (id == null)
@@ -44,9 +42,6 @@ namespace MvcMovie.Controllers
             return View();
         }
 
-        // POST: Movies/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Create([Bind("Id,Title,ReleaseDate,Genre,Price")] Movie movie)
@@ -60,7 +55,6 @@ namespace MvcMovie.Controllers
             return View(movie);
         }
 
-        // GET: Movies/Edit/5
         public IActionResult Edit(Guid? id)
         {
             if (id == null)
@@ -76,9 +70,6 @@ namespace MvcMovie.Controllers
             return View(movie);
         }
 
-        // POST: Movies/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Edit(Guid id, [Bind("Id,Title,ReleaseDate,Genre,Price")] Movie movie)
@@ -119,7 +110,6 @@ namespace MvcMovie.Controllers
             return View(movie);
         }
 
-        // POST: Movies/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public IActionResult DeleteConfirmed(Guid id)
